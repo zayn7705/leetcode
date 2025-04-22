000c1d610a36a256f3dc0b7b8b256663c2151235
zayn7705 <zayn7705@gmail.com>
2025-04-21 23:00:36 -0400

    283. Move Zeroes

---
+++ moveZeroes.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) <= 1:
+            return nums
+        counter = 0
+        for i in nums:
+            if i != 0:
+                nums[counter] = i
+                counter += 1
+        while counter < len(nums):
+            nums[counter] = 0
+            counter += 1
