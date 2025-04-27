271a4fe93beb3a9c9cc669ab4a18869dc27f9632
zayn7705 <zayn7705@gmail.com>
2025-04-26 23:56:44 -0400

    1768. Merge Strings Alternately

---
+++ mergeAlternatively.py
@@ -0,0 +1,19 @@
+# Time complexity: O(n)
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        mergedWord = ""
+        
+        for i in range(max(len(word1), len(word2))):
+            if i < len(word1):
+                mergedWord += word1[i]
+
+            if i < len(word2):
+                mergedWord += word2[i]
+
+        return mergedWord
+        
